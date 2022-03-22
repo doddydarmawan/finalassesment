16a3799e1bfe5c4467c84058b41985e0d01c355c
izzahsirclo <muhammad.izzah@sirclo.com>
2022-03-22 01:57:12 +0000

    feat(26630828): doing no 1, create upgrade schema etc.

---
+++ app/code/Icube/OkrDivisiCustomSyaiful/Setup/UpgradeSchema.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Icube\OkrDivisiCustomSyaiful\Setup;
+
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Media;
+use Magento\Catalog\Model\Product\Attribute\Backend\Media\ImageEntryConverter;
+use Magento\Framework\DB\Ddl\Table;
+
+/**
+ * Upgrade the Catalog module DB scheme
+ */
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '1.0.1') < 0) {
+            $table = $setup->getConnection()
+                ->newTable($setup->getTable('icube_acara_syaiful'))
+                ->addColumn(
+                    'entity_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['primary' => true, 'identity' => true, 'nullable' => false],
+                    'Smallint'
+                )->addColumn(
+                    'nama',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'Nama acara'
+                )->addColumn(
+                    'pemateri',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    50,
+                    [],
+                    'pemateri acara'
+                )->setComment("Icube Acara");
+            $setup->getConnection()->createTable($table);
+
+            $table = $setup->getConnection()
+                ->newTable($setup->getTable('icube_peserta_syaiful'))
+                ->addColumn(
+                    'entity_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['primary' => true, 'identity' => true, 'nullable' => false],
+                    'Smallint'
+                )->addColumn(
+                    'nama',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'Nama peserta'
+                )->addColumn(
+                    'acara_id',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                    null,
+                    ['nullable' => false, 'primary' => true],
+                    'pemateri peserta'
+                )->addIndex(
+                    $setup->getIdxName('icube_peserta_syaiful', ['acara_id']),
+                    ['acara_id']
+                )->addForeignKey(
+                    $setup->getFkName('icube_peserta_syaiful', 'acara_id', 'icube_acara_syaiful', 'entity_id'),
+                    'acara_id',
+                    $setup->getTable('icube_acara_syaiful'),
+                    'entity_id',
+                    \Magento\Framework\DB\Ddl\Table::ACTION_NO_ACTION
+                )->setComment("Icube peserta");
+            $setup->getConnection()->createTable($table);
+        }
+
+        $setup->endSetup();
+    }
+}
---
+++ app/code/Icube/OkrDivisiCustomSyaiful/registration.php
@@ -0,0 +1,5 @@
+<?php 
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+		'Icube_OkrDivisiCustomSyaiful',__DIR__
+);
